d3a03fdd3ef2f91a0514fc2727d162f8b600fca2
Kholifatulardhi08 <ardhi.kholofatul8@gmail.com>
2023-07-25 13:39:49 +0700

    styling for mailtrap konfirmasi
    Signed-off-by: Kholifatulardhi08 <ardhi.kholofatul8@gmail.com>

--- resources/views/email/confirmation.blade.php
+++ resources/views/email/confirmation.blade.php
@@ -3,47 +3,83 @@
 
 <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Welcome to E-service</title>
+    <style>
+        /* Add your custom CSS styles here */
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+        }
+
+        table {
+            width: 100%;
+            max-width: 600px;
+            margin: 0 auto;
+        }
+
+        td {
+            padding: 10px;
+        }
+
+        .header {
+            background-color: #f2f2f2;
+            text-align: center;
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        .content {
+            background-color: #ffffff;
+            border: 1px solid #e0e0e0;
+        }
+
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: #ffffff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+
+        .footer {
+            background-color: #f2f2f2;
+            text-align: center;
+            font-size: 12px;
+        }
+    </style>
 </head>
 
 <body>
-    <tr>
-        <td>
-            Dear {{ $nama }}!
-        </td>
-    </tr>
-    <tr>
-        <td>
-            &nbsp;
-        </td>
-    </tr>
-    <tr>
-        <td>
-            please click on bellow to active your penyedia account: -
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <a href="{{ url('penyedia/confirm/'.$code) }}">
-                {{ url('penyedia/confirm/'.$code) }}
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            &nbsp;
-        </td>
-    </tr>
-    <tr>
-        <td>
-            thanks
-        </td>
-    </tr>
-    <tr>
-        <td>
-            E-service
-        </td>
-    </tr>
+    <table cellspacing="0" cellpadding="0">
+        <tr>
+            <td class="header">
+                Welcome to E-service
+            </td>
+        </tr>
+        <tr>
+            <td class="content">
+                <p>Dear {{ $nama }},</p>
+                <p>Please click below to activate your penyedia account:</p>
+                <p>
+                    <a class="button" href="{{ url('penyedia/confirm/'.$code) }}">
+                        Activate Account
+                    </a>
+                </p>
+                <p>If the button above does not work, you can also copy and paste the following link into your browser:</p>
+                <p>
+                    {{ url('penyedia/confirm/'.$code) }}
+                </p>
+                <p>Thank you,</p>
+                <p>E-service</p>
+            </td>
+        </tr>
+        <tr>
+            <td class="footer">
+                &copy; 2023 E-service. All rights reserved.
+            </td>
+        </tr>
+    </table>
 </body>
 
 </html>
--- resources/views/email/confirmed.blade.php
+++ resources/views/email/confirmed.blade.php
@@ -3,60 +3,97 @@
 
 <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Penyedia Confirmation</title>
+    <style>
+        /* Add your custom CSS styles here */
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+        }
+
+        table {
+            width: 100%;
+            max-width: 600px;
+            margin: 0 auto;
+        }
+
+        td {
+            padding: 10px;
+        }
+
+        .header {
+            background-color: #f2f2f2;
+            text-align: center;
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        .content {
+            background-color: #ffffff;
+            border: 1px solid #e0e0e0;
+        }
+
+        .details {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        .details th,
+        .details td {
+            border: 1px solid #e0e0e0;
+            padding: 8px;
+            text-align: left;
+        }
+
+        .details th {
+            background-color: #f2f2f2;
+        }
+
+        .footer {
+            background-color: #f2f2f2;
+            text-align: center;
+            font-size: 12px;
+        }
+    </style>
 </head>
 
 <body>
-    <tr>
-        <td>
-            Dear {{ $nama }}!
-        </td>
-    </tr>
-    <tr>
-        <td>
-            &nbsp;
-        </td>
-    </tr>
-    <tr>
-        <td>
-           Your penyedia is confirmed, you can login and add details personal bank and jasa. 
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Your Details as bellow :
-        </td>
-    </tr>
-    <tr>
-        <td>
-            &nbsp;
-        </td>
-    </tr>
-    <tr>
-        <td>
-            name = {{ $nama }}
-        </td>
-    </tr>
-    <tr>
-        <td>
-            no handphone = {{ $no_hp }}
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Email = {{ $email }}
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Thanks
-        </td>
-    </tr>
-    <tr>
-        <td>
-            E-service
-        </td>
-    </tr>
+    <table cellspacing="0" cellpadding="0">
+        <tr>
+            <td class="header">
+                Penyedia Confirmation
+            </td>
+        </tr>
+        <tr>
+            <td class="content">
+                <p>Dear {{ $nama }},</p>
+                <p>Your penyedia account is confirmed. You can now log in and add your personal bank and jasa details.
+                </p>
+                <p>Your Details:</p>
+                <table class="details">
+                    <tr>
+                        <th>Name</th>
+                        <td>{{ $nama }}</td>
+                    </tr>
+                    <tr>
+                        <th>No Handphone</th>
+                        <td>{{ $no_hp }}</td>
+                    </tr>
+                    <tr>
+                        <th>Email</th>
+                        <td>{{ $email }}</td>
+                    </tr>
+                </table>
+                <p>Thank you,</p>
+                <p>E-service</p>
+            </td>
+        </tr>
+        <tr>
+            <td class="footer">
+                &copy; 2023 E-service. All rights reserved.
+            </td>
+        </tr>
+    </table>
 </body>
 
 </html>
