3283268f1729e6c98c75373bd966b915a4820c2c
Kholifatulardhi08 <ardhi.kholofatul8@gmail.com>
2023-08-18 22:52:01 +0700

    percantik dikit
    Signed-off-by: Kholifatulardhi08 <ardhi.kholofatul8@gmail.com>

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -1,129 +1,27 @@
-<footer class="footer">
-    <div class="container">
-        <!-- Mid-Footer -->
-        {{--  <div class="mid-footer-wrapper u-s-p-b-80">
-            <div class="row">
-                <div class="col-lg-3 col-md-3 col-sm-12">
-                    <div class="footer-list">
-                        <h6>COMPANY</h6>
-                        <ul>
-                            <li>
-                                <a href="/">About Us</a>
-                            </li>
-                            <li>
-                                <a href="/">Contact Us</a>
-                            </li>
-                            <li>
-                                <a href="/">FAQ</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-12">
-                    <div class="footer-list">
-                        <h6>COLLECTION</h6>
-                        <ul>
-                            <li>
-                                <a href="cart.html">Men Clothing</a>
-                            </li>
-                            <li>
-                                <a href="checkout.html">Women Clothing</a>
-                            </li>
-                            <li>
-                                <a href="account.html">Kids Clothing</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-12">
-                    <div class="footer-list">
-                        <h6>ACCOUNT</h6>
-                        <ul>
-                            <li>
-                                <a href="shop-v1-root-category.html">My Account</a>
-                            </li>
-                            <li>
-                                <a href="shop-v1-root-category.html">My Profile</a>
-                            </li>
-                            <li>
-                                <a href="listing.html">My Orders</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-12">
-                    <div class="footer-list">
-                        <h6>Contact</h6>
-                        <ul>
-                            <li>
-                                <i class="fas fa-location-arrow u-s-m-r-9"></i>
-                                <span>E-service</span>
-                            </li>
-                            <li>
-                                <a href="tel:+111-222-333">
-                                    <i class="fas fa-phone u-s-m-r-9"></i>
-                                    <span>+6289658662848</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="ardhi.kholifatul8@gmail.com">
-                                    <i class="fas fa-envelope u-s-m-r-9"></i>
-                                    <span>
-                                        ardhi.kholifatul8@gmail.com</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>  --}}
-        <!-- Mid-Footer /- -->
-        <!-- Bottom-Footer -->
-        <div class="bottom-footer-wrapper">
-            <div class="social-media-wrapper">
-                <ul class="social-media-list">
-                    <li>
-                        <a href="#">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fab fa-google-plus-g"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fas fa-rss"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fab fa-pinterest"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fab fa-linkedin-in"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fab fa-youtube"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <p class="copyright-text">Copyright &copy; 2023
-                <a target="_blank" rel="nofollow">E-service</a> | All
-                Right Reserved
-            </p>
+<section class="">
+    <!-- Footer -->
+    <footer class="text-center text-white" style="background-color: #ac0202;">
+        <!-- Grid container -->
+        <div class="container p-4 pb-0">
+            <!-- Section: CTA -->
+            <section class="">
+                <p class="d-flex justify-content-center align-items-center">
+                    <span class="me-3">Register for free</span>
+                    <button type="button" class="btn btn-outline-light btn-rounded">
+                        Sign up!
+                    </button>
+                </p>
+            </section>
+            <!-- Section: CTA -->
         </div>
-    </div>
-    <!-- Bottom-Footer /- -->
-</footer>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2020 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+</section>
