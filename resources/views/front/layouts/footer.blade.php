478ff29fba24826eee714bc28385da288d1d731b
Kholifatulardhi08 <ardhi.kholofatul8@gmail.com>
2023-08-18 22:55:18 +0700

    tambah dikit
    Signed-off-by: Kholifatulardhi08 <ardhi.kholofatul8@gmail.com>

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -1,25 +1,10 @@
 <section class="">
     <!-- Footer -->
     <footer class="text-center text-white" style="background-color: #ac0202;">
-        <!-- Grid container -->
-        <div class="container p-4 pb-0">
-            <!-- Section: CTA -->
-            <section class="">
-                <p class="d-flex justify-content-center align-items-center">
-                    <span class="me-3">Register for free</span>
-                    <button type="button" class="btn btn-outline-light btn-rounded">
-                        Sign up!
-                    </button>
-                </p>
-            </section>
-            <!-- Section: CTA -->
-        </div>
-        <!-- Grid container -->
-
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2020 Copyright:
-            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+            © Skripsi A Kholifatul Ardli - 2023 Copyright:
+            <a class="text-white" href="/">E-service</a>
         </div>
         <!-- Copyright -->
     </footer>
